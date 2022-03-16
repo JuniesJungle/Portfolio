f189b7b9b27ebc19c923f048f74e768345443fde
JuniesJungle <juniper.peasley@gmail.com>
2022-03-16 01:48:49 -0600

    Fixed buttons (no links yet) and Nav Bar to good style and added a banner

---
+++ javascript.js
@@ -0,0 +1,20 @@
+/* For Navbar Settings Dropdown
+When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+  document.getElementById('myDropdown').classList.toggle('show');
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function (event) {
+  if (!event.target.matches('.navbar-link')) {
+    var dropdowns = document.getElementsById('myDropdown');
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+};
